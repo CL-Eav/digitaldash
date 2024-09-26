17093bb9eb6fdd74e51858273e3e433962c706d2
CL-Eav <147419233+CL-Eav@users.noreply.github.com>
2024-09-26 13:38:26 +0100

    updated the alert

--- digitaldash/keLabel.py
+++ digitaldash/keLabel.py
@@ -30,7 +30,7 @@ class KELabel(Label):
         self.default = args.get("default", "")
         self.configColor = args.get("color", (1, 1, 1, 1))  # White
         self.color = self.configColor
-        self.configFontSize = args.get("font_size", 25)
+        self.configFontSize = args.get("font_size", 100)   # Default font size (Alert Message Text)
         self.font_size = self.configFontSize
         self.pid = args.get("pid", None)
         self.decimals = (
